d5b736df88de11a6320f6831d015a61487a05752
Alim1899 <alimakhchanogli@gmail.com>
2024-04-08 13:59:47 +0400

    Added Footer and header logo

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Footer.module.css'
+const Footer = () => {
+  return (
+    <div className={classes.main}>
+    <div className={classes.gradient}></div>
+      <p className={classes.text}>MKN Computer Bilgisayar, Yazılım ve Chiptuning Hizmetleri © 2023 | Tüm hakları saklıdır.</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,28 @@
-import Navbar from "../Navbar/Navbar"
-import Home from "../Pages/Home/Home"
+import Navbar from "../Navbar/Navbar";
+import Home from "../Pages/Home/Home";
+import { Fragment } from "react";
+import Footer from "../Footer/Footer";
 import classes from './Layout.module.css'
+import logo from '../../assets/logo.png'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 const Layout = () => {
   return (
-    <div className={classes.main}>
-      <Navbar/>
-      <Home/>
-    </div>
-  )
-}
+    <Fragment>
+    <div className={classes.header}>
+    <img className={classes.logo} alt="logo" src={logo}></img>
 
-export default Layout
+    </div>
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Layout;
